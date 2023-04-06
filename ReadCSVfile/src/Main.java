75e78ed2dafb5dab8e7a6c4de4f56d9eff184574
aasimsyed97 <sayedaasim555@gmail.com>
2023-04-06 11:19:43 +0530

    project intialization

---
+++ ReadCSVfile/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String CSVinput = sc.nextLine();
+
+
+         csvOutput(CSVinput);
+
+    }
+
+    public static void csvOutput(String string){
+         System.out.println("required output");
+    }
+}
